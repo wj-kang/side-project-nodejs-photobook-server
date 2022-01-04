955b444fb22f3434ee2c384f693750bcf4b4c1bc
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-01-04 13:39:21 -0600

    Add jsonwebtoken helper functions

---
+++ utilities/jwt.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+  createSignedToken: (userId) => {
+    return jwt.sign(
+      {
+        id: userId,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: '1h' }
+    );
+  },
+  decodeToken: (token) => {
+    return jwt.decode(token);
+  },
+  verifyTokenMiddleware: (req, res, next) => {
+    try {
+      const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET_KEY);
+      req.userId = decoded.id;
+      next();
+    } catch (error) {
+      return res.status(401).json({ error: '401 Unauthorized' });
+    }
+  },
+};
