e5e5d2c5a9f76f6d93090682c853af441f85c192
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-01-19 14:22:45 -0600

    Add github OAuth controller

---
+++ controller/auth/github.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+const jwtUtility = require('../../utilities/jwt');
+const client_id = process.env.GITHUB_CLIENT_ID;
+const client_secret = process.env.GITHUB_CLIENT_SECRET;
+const { User } = require('../../models');
+const { createSampleData } = require('../../utilities/sampleData');
+
+module.exports = async (req, res) => {
+  const { code } = req.query;
+  try {
+    const {
+      data: { access_token },
+    } = await axios({
+      method: 'post',
+      url: `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}`,
+      headers: {
+        accept: 'application/json',
+      },
+    });
+
+    const getData = await axios.get('https://api.github.com/user/emails', {
+      headers: {
+        Authorization: `token ${access_token}`,
+      },
+    });
+
+    const user = await User.findOrCreate({
+      where: {
+        email: getData.data[0].email,
+        type: 'GITHUB',
+      },
+      defaults: {
+        username: getData.data[0].email.split('@')[0],
+        password: process.env.DEFAULT_PASSWORD,
+      },
+    });
+
+    const token = jwtUtility.createSignedToken(user[0].id);
+    // create sample data
+    await createSampleData(user[0].id);
+
+    res
+      .status(302)
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        domain: '.wonjunkang.com',
+      })
+      .redirect(`${process.env.CLIENT_BASE_URL}/main/albums`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
+};
