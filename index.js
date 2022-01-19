36c1170cae09dde1a5907d7f4d98657bbd027813
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-01-18 23:36:05 -0600

    revise index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const jwtUtility = require('./utilities/jwt');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middlewares
+app.use(
+  express.json(),
+  express.urlencoded({ extended: false }),
+  cookieParser(),
+  morgan(`${process.env.NODE_ENV === 'production' ? 'combined' : 'dev'}`),
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+// Routing
+app.get('/', (req, res) => {
+  res.status(200).send('server on');
+});
+app.use('/auth', require('./routes/auth'));
+
+app.use((req, res, next) => {
+  jwtUtility.verifyTokenMiddleware(req, res, next);
+});
+
+app.use('/user', require('./routes/user'));
+app.use('/album', require('./routes/album'));
+app.use('/post', require('./routes/post'));
+
+// 404 Not Found
+app.use((req, res, next) => {
+  res.status(404).send('ERROR(404) Page Not Found');
+});
+
+// Serve
+app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
