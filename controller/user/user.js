7cc3d8c8163ebd8c31fe41c4500437520bce4c17
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-01-19 20:24:22 -0600

    Add controller - get userInfo

--- controller/user/user.js
+++ controller/user/user.js
@@ -1,6 +1,18 @@
 const { User } = require('../../models');
 
 module.exports = {
+  get: async (req, res) => {
+    const userId = req.userId;
+    if (!userId) {
+      return res.status(400).end();
+    }
+    let user = await User.findOne({
+      where: { id: userId },
+    });
+    user = user.toJSON();
+    const { email, type } = user;
+    res.status(200).json({ email, type });
+  },
   delete: async (req, res) => {
     const userId = req.params;
     try {
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controller/user');
 
 // User
+router.get('/', userController.user.get);
 router.delete('/', userController.user.delete);
 
 module.exports = router;
